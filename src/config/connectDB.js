4f546d7cd31f2140fbdc667c32bb6aa792511cf8
Maybetuandat <trinhvinhtuandat05102003@gmail.com>
2024-08-01 21:16:11 +0700

    dat update file env

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,10 +1,15 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
-const sequelize = new Sequelize("aitcvnkd_hoanghiep2", "aitcvnkd_hoanghiep", "@Hiep2003", {
-  host: "localhost",
-  port: "3306",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    port: "3306",
+    dialect: "mysql",
+  }
+);
 const connection = async () => {
   try {
     await sequelize.authenticate();
