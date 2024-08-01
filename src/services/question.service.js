1454fc42c73e79d1d93f73e261f078da4e637fae
Hoàng Anh Vũ <109050725+hoanganhvu271@users.noreply.github.com>
2024-08-01 17:11:33 +0700

    vu fix admin 2

--- src/services/question.service.js
+++ src/services/question.service.js
@@ -116,10 +116,11 @@ const getQuestionOfTestAdmin = async (id) => {
       const { MaBaiThi, ...rest } = question;
       return rest;
     });
-    questions = shuffleLast10KeepRest(questions);
+    // questions = shuffleLast10KeepRest(questions);
     if (questions.length > 0) {
       data.status = 200;
       data.data = questions;
+      console.log(questions)
     } else {
       data.status = 404;
     }
