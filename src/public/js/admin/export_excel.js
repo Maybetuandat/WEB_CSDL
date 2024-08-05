c7c50ef620a6f48cfb4985baf23c60fba16fd35e
Maybetuandat <trinhvinhtuandat05102003@gmail.com>
2024-08-05 13:32:11 +0700

    dat them xuat excel

--- src/public/js/admin/export_excel.js
+++ src/public/js/admin/export_excel.js
@@ -1,54 +1,10 @@
-// var studentList = JSON.parse(
-//   document.querySelectorAll("input")[1].getAttribute("data_to_excel_student")
-// );
-// var resultList = JSON.parse(
-//   document.querySelectorAll("input")[2].getAttribute("data_to_excel_result")
-// );
-// function roundToDecimalPlace(value, decimals) {
-//   const factor = Math.pow(10, decimals);
-//   return Math.round(value * factor) / factor;
-// }
-
-// document.getElementById("exportButton").addEventListener("click", async () => {
-//   // Tạo workbook và worksheet
-//   const workbook = new ExcelJS.Workbook();
-//   const worksheet = workbook.addWorksheet("Sheet1");
-
-//   // Định nghĩa các cột
-//   worksheet.columns = [
-//     { header: "STT", key: "STT", width: 5 },
-//     { header: "MSV", key: "MSV", width: 15 },
-//     { header: "Tên", key: "Tên", width: 30 },
-//     { header: "Điểm", key: "Điểm", width: 10 },
-//   ];
-
-//   // Thêm dữ liệu vào worksheet
-//   for (var i = 0; i < studentList.length; i++) {
-//     worksheet.addRow({
-//       MSV: studentList[i].TaiKhoan,
-//       Tên: studentList[i].Ten,
-//       Điểm: roundToDecimalPlace(resultList[i].Diem, 0),
-//     });
-//   }
-//   // Xuất workbook ra file Excel
-//   const buffer = await workbook.xlsx.writeBuffer();
-//   const blob = new Blob([buffer], {
-//     type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-//   });
-//   const url = URL.createObjectURL(blob);
-//   const a = document.createElement("a");
-//   a.href = url;
-//   a.download = "student_list.xlsx";
-//   a.click();
-//   URL.revokeObjectURL(url);
-// });
 var studentList = JSON.parse(
   document.querySelectorAll("input")[1].getAttribute("data_to_excel_student")
 );
 var resultList = JSON.parse(
   document.querySelectorAll("input")[2].getAttribute("data_to_excel_result")
 );
-
+var nameTest = document.querySelectorAll("input")[3].getAttribute("name_excel");
 function roundToDecimalPlace(value, decimals) {
   const factor = Math.pow(10, decimals);
   return Math.round(value * factor) / factor;
@@ -70,7 +26,7 @@ document.getElementById("exportButton").addEventListener("click", async () => {
     {
       header: "MSV",
       key: "MSV",
-      width: 15,
+      width: 20,
       alignment: { vertical: "middle", horizontal: "center" },
     },
     {
@@ -87,6 +43,7 @@ document.getElementById("exportButton").addEventListener("click", async () => {
     },
   ];
 
+  worksheet.getRow(1).alignment = { vertical: "middle", horizontal: "center" };
   // Thêm dữ liệu vào worksheet
   for (var i = 0; i < studentList.length; i++) {
     worksheet
@@ -109,7 +66,8 @@ document.getElementById("exportButton").addEventListener("click", async () => {
   const url = URL.createObjectURL(blob);
   const a = document.createElement("a");
   a.href = url;
-  a.download = "student_list.xlsx";
+  a.download = nameTest.substring(1, nameTest.length - 1) + ".xlsx";
+  console.log(typeof nameTest);
   a.click();
   URL.revokeObjectURL(url);
 });
