977c2902c62404beedf33dd05ed8ae9790d015d1
Maybetuandat <trinhvinhtuandat05102003@gmail.com>
2024-08-07 14:24:19 +0700

    dat update

--- src/routes/admin/shift.router.js
+++ src/routes/admin/shift.router.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const fileUploader = require('../../config/cloudinary.config');
+const fileUploader = require("../../config/cloudinary.config");
 const controllerShift = require("../../controllers/admin/shift.controller");
 
-
-
 const { isAdmin } = require("../../middleware/auth.middleware");
-
-
 
 router.get("/", controllerShift.shiftListPaginate);
 router.get("/new", controllerShift.createNewShift);
@@ -16,5 +12,3 @@ router.post("/api/edit-shift", controllerShift.updateShift);
 router.post("/api/new-shift", controllerShift.createShift);
 
 module.exports = router;
-
-
