5e0f7194d03138ddc3b85a60695bb28211a9d21a
Maybetuandat <trinhvinhtuandat05102003@gmail.com>
2024-08-08 08:35:19 +0700

    dat update

--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -7,14 +7,14 @@ const { isAuth } = require("../../middleware/auth.middleware");
 router.get("/", isAuth, User.index);
 router.get("/login", User.userLogin);
 router.post("/checkLoginUser", checkLoginUser);
-router.get("/register", User.register);
-router.post("/register/createNewUser", User.createUser);
-router.get("/forgotPassword", User.forgotPassword_index);
-router.post("/forgotPassword/resetPassword", User.forgotPassword);
-router.post("/forgotPassword/resetPassword/verifyOTP", User.verifyOTP);
+// router.get("/register", User.register);
+// router.post("/register/createNewUser", User.createUser);
+// router.get("/forgotPassword", User.forgotPassword_index);
+// router.post("/forgotPassword/resetPassword", User.forgotPassword);
+// router.post("/forgotPassword/resetPassword/verifyOTP", User.verifyOTP);
 
-router.post(
-  "/forgotPassword/resetPassword/verifyOTP/newPassword",
-  User.changePassword
-);
+// router.post(
+//   "/forgotPassword/resetPassword/verifyOTP/newPassword",
+//   User.changePassword
+// );
 module.exports = router;
